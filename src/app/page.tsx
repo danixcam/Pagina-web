f9532ccc7ee4e31b8789bbea66d48f4cb0130d01
danixcam <161505059+danixcam@users.noreply.github.com>
2025-08-29 23:03:49 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -263,7 +263,7 @@ export default function Home() {
       </footer>
 
       {/* Font Awesome Script */}
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+      <script async src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
     </div>
   );
 }
